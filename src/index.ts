e005a9f88a8a2554c577dba99e402643cb12c374
alexjr101 <alex.j.ribes@gmail.com>
2024-03-27 13:01:27 -0500

    Login endpoints, DynamoDB connectoin, CORS

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,33 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import AuthRouter from "./routes/auth";
+import createHttpError, { isHttpError } from "http-errors";
 
 const app = express();
 const port = 5005;
 
+const cors = require('cors');
+
 // To use json:
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173',
+}));
 
-app.get("/api/v0", (req, res) => {
-  res.json({
-    msg: "Hello World!"
-  })
+app.use("/api/v0/auth", AuthRouter);
+
+app.use((req, res, next) => {
+	next(createHttpError(404, "Endpoint not found"));
+})
+
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+	console.error(error);
+	let errorMessage = "Whoops, something went wrong";
+	let statusCode = 500;
+	if (isHttpError(error)) {
+		statusCode = error.status;
+		errorMessage = error.message;
+	}
+	res.status(statusCode).json({ error: errorMessage });
 });
 
 app.listen(port, () => {
---
+++ src/routes/auth.ts
@@ -0,0 +1,130 @@
+import { Router } from "express";
+import { DynamoDBClient, } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, GetCommand, PutCommand, QueryCommand, ScanCommand } from "@aws-sdk/lib-dynamodb";
+import createHttpError from "http-errors";
+import crypto from 'crypto';
+import bcrypt, { hash } from 'bcrypt';
+
+const AuthRouter = Router();
+
+const client = new DynamoDBClient({
+  region: "us-east-1"
+});
+
+const docClient = DynamoDBDocumentClient.from(client);
+
+AuthRouter.post("/login", async (req, res, next) => {
+
+  try {
+    const { username, password } = req.body;
+
+    if (!username) {
+      throw createHttpError(400, "Username required in request body");
+    }
+    
+    if (!password) {
+      throw createHttpError(400, "Password required in request body");
+    }
+
+    const getResponse = await docClient.send(
+      new ScanCommand({
+        TableName: "users",
+        FilterExpression: "username = :username",
+        ExpressionAttributeValues: {
+          ":username": username
+         }
+      })
+    ); 
+
+    const results = getResponse.Items;
+
+    if (!results) {
+      throw createHttpError(403, "Username or password incorrect");
+    }
+
+    const user = results[0]
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw createHttpError(403, "Username or password incorrect");
+    }
+
+    res.json({
+      msg: "Logged in!",
+      user
+    });
+
+  }
+
+  catch(error) {
+    next(error);
+  }
+})
+
+AuthRouter.post("/register", async (req, res, next) => {
+  try {
+    const { firstName, lastName, role, username, password, email } = req.body;
+
+    if (!firstName) {
+      throw createHttpError(400, "First Name required in request body")
+    }
+
+    if (!lastName) {
+      throw createHttpError(400, "Last Name required in request body")
+    }
+
+    if (!role) {
+      throw createHttpError(400, "Role required in request body");
+    }
+
+    if (!username) {
+      throw createHttpError(400, "Username required in request body");
+    }
+    
+    if (!password) {
+      throw createHttpError(400, "Password required in request body");
+    }
+
+    if (!email) {
+      throw createHttpError(400, "Email required in request body");
+    }
+
+    const userID = crypto.randomUUID();
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const putCommand = new PutCommand({
+      TableName: "users",
+      Item: {
+        id: userID,
+        firstName,
+        lastName,
+        role,
+        username,
+        password: hashedPassword,
+        email
+      }
+    });
+
+    // No error means the PUT command was successful
+    await docClient.send(putCommand);
+
+    const getResponse = await docClient.send(
+      new GetCommand({
+        TableName: "users",
+        Key: {
+          id: userID
+        }
+      })
+    ); 
+
+    console.log(getResponse);
+    res.status(200).json({
+      result: getResponse
+    });
+  } catch(error) {
+    next(error);
+  }
+})
+
+export default AuthRouter;
